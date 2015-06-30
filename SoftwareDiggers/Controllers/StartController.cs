5a486eb726540a6fae9edbadc8e8776894441ce9
julius.rachmanas <julius.rachmanas@atea.lt>
2015-06-30 20:17:16 +0300

    Biski ne visai sekmingai apipavidalintas

---
+++ SoftwareDiggers/Controllers/StartController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace SoftwareDiggers.Controllers
+{
+    public class StartController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+         
+    }
+}
