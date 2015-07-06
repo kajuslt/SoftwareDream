1a58ee7fa66bdee6c0b9f5c3f9966d4a001a88d0
julius.rachmanas <julius.rachmanas@atea.lt>
2015-07-06 14:01:43 +0300

    Commit Models

--- SoftwareDiggers/Models/Facebook.cs
+++ SoftwareDiggers/Models/Facebook.cs
@@ -2,6 +2,9 @@
 {
     public class Facebook
     {
-        public int Id { get; set; } 
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public int FacebookUserId { get; set; }
+        public string FacebookAccessToken { get; set; }
     }
 }
--- SoftwareDiggers/Models/Twitter.cs
+++ SoftwareDiggers/Models/Twitter.cs
@@ -2,6 +2,10 @@
 {
     public class Twitter
     {
-        public int Id { get; set; } 
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public int TwitterUserId { get; set; }
+        public string TwitterAccessToken { get; set; }
+        public string TwitterDisplayName { get; set; }
     }
 }
