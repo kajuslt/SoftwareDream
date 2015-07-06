1321aa798d9f893914dfc89e617848bdafcbc46a
julius.rachmanas <julius.rachmanas@atea.lt>
2015-07-06 13:40:57 +0300

    Uzpildyti Modeliai

--- SoftwareDiggers/Models/Donations.cs
+++ SoftwareDiggers/Models/Donations.cs
@@ -1,9 +1,13 @@
-﻿namespace SoftwareDiggers.Models
+﻿using System;
+
+namespace SoftwareDiggers.Models
 {
     public class Donations
     {
         public int Id { get; set; }
         public int UserId { get; set; }
         public int ProjectId { get; set; }
+        public DateTime Date { get; set; }
+        public decimal AmmountOfDonation { get; set; }
     }
 }
