b79c564cb0247ec5713805df1a7c37ae2984b9ee
julius.rachmanas <julius.rachmanas@atea.lt>
2015-08-31 08:26:17 +0300

    CSS ir Js

--- SoftwareDiggers/Scripts/app/Controllers/StartController.js
+++ SoftwareDiggers/Scripts/app/Controllers/StartController.js
@@ -1,8 +1,23 @@
-﻿var StartController = function ($scope, $controller) {
+﻿var StartController = function ($scope, $controller, $http) {
     $controller('BaseController', { $scope: $scope });
     $scope.items = ['_comonData', '_platforms', '_shortDescription'];
     $scope.selection = $scope.items[0];
+    $scope.projectInfoForm = {
+        projectNameString: "default",
+        mainPurposeString: "default",
+        additionalPurposeString: "default",
+        basePlatform: { id: 0 },
+        os: { id: 0 },
+        browser: { id: 0 },
+        expectationsString: "default"
+    };
+    $scope.submitForm = function (url) {
+        if ($scope.projectInfoForm.$valid) {
+            $http.post(url, JSON.stringify($scope.projectInfoForm)).success(function() {});
+        }
+    };
+
 }
 
 // The inject property of every controller (and pretty much every other type of object in Angular) needs to be a string array equal to the controllers arguments, only as strings
-StartController.$inject = ['$scope', '$controller'];
+StartController.$inject = ['$scope', '$controller','$http'];
